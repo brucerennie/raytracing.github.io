85788e1e205a8d520f36709f01fd977878d34000
Steve Hollasch <steve@hollasch.net>
2021-07-08 16:20:05 -0700

    s/Update src to proper projection code
    Following the latest non-orthogonal basis vector calculations.

--- src/TheNextWeek/quad.h
+++ src/TheNextWeek/quad.h
@@ -21,11 +21,10 @@ class quad : public hittable {
     quad(const point3& o, const vec3& aa, const vec3& ab, shared_ptr<material> m)
       : plane_origin(o), axis_A(aa), axis_B(ab), mat(m)
     {
-        normal = unit_vector(cross(axis_A, axis_B));
+        auto n = cross(axis_A, axis_B);
+        normal = unit_vector(n);
         D = -dot(normal, plane_origin);
-
-        measure_A = axis_A / dot(axis_A, axis_A);
-        measure_B = axis_B / dot(axis_B, axis_B);
+        L = n / dot(n,n);
 
         set_bounding_box();
     }
@@ -63,8 +62,8 @@ class quad : public hittable {
         // Determine the hit point lies within the planar shape using its plane coordinates.
         auto intersection = r.at(t);
         vec3 planar_hitpt_vector = intersection - plane_origin;
-        auto a = dot(planar_hitpt_vector, measure_A) ;
-        auto b = dot(planar_hitpt_vector, measure_B);
+        auto a = dot(L, cross(planar_hitpt_vector, axis_B));
+        auto b = dot(L, cross(axis_A, planar_hitpt_vector));
 
         if (!hit_ab(a, b, rec))
             return false;
@@ -84,7 +83,7 @@ class quad : public hittable {
     shared_ptr<material> mat;
     vec3 normal;
     double D;
-    vec3 measure_A, measure_B;
+    vec3 L;
     aabb bbox;
 };
 
